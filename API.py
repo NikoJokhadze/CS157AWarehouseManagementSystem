688b0ba2ef4fd1f7c36546c76d1be7980501c207
NikoJokhadze <ngjokhadze@hotmail.com>
2024-04-29 01:49:23 -0700

    Added get methods for warehouse, orders, and items, as well as an update method for warehouse and a delete method for orders

--- API.py
+++ API.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 from flask import Flask, jsonify, request
 from getpass import getpass
 
-app = Flask(__name__)
+
 try:  # Surrounding the connection in a try-except block to catch all connection errors
     password = getpass("Enter your password for MySQL: ")
     conn = mysql.connector.connect(user="root", password=password,
@@ -18,9 +18,10 @@ except mysql.connector.Error as err:
 
 print(conn)  # Printing the successful connection
 
+app = Flask(__name__)
 
-@app.route("/employee/get_all", method=['GET'])
-def get_all():
+@app.route("/employee/get_all", methods=['GET'])
+def get_users():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Employee")
     data = cursor.fetchall()
@@ -31,7 +32,7 @@ def get_all():
         return jsonify(data), 200
 
 
-@app.route("/employee/create_user", method=['POST'])
+@app.route("/employee/create_user", methods=['POST'])
 def create_user():
     cursor = conn.cursor()
     data = request.json
@@ -60,5 +61,116 @@ def create_user():
     except Exception as e:
         return jsonify({"message": f"Failed to create a new user for {new_first} {new_last}", "error": str(e)}), 500
 
+
+@app.route("/warehouse/get_warehouses", methods=['GET'])
+def get_warehouses():
+    cursor = conn.cursor()
+    cursor.execute("SELECT w.warehouseID, w.capacity, a.addressNum, a.street, a.city, a.zipcode "
+                   "FROM warehouse w "
+                   "INNER JOIN addresses a "
+                   "ON w.warehouseAddressID = a.addressID")
+    data = cursor.fetchall()
+    cursor.close()
+    if len(data) == 0:
+        return jsonify({'message': "There are no warehouses in the system."})
+    else:
+        return jsonify(data), 200
+
+
+@app.route('/warehouse/update_warehouse', methods=['PUT'])
+def update_warehouse():
+    data = request.json
+    warehouseID = data[0]
+    warehouseAddressID = data[1]
+    capacity = data[2]
+    addressNum = data[3]
+    street = data[4]
+    city = data[5]
+    zipCode = data[6]
+
+    if warehouseID is None or warehouseAddressID is None or capacity is None or addressNum is None\
+            or capacity is None or street is None or city is None or zipCode is None:
+        return jsonify({'message': 'To update the warehouse, you must provide the current warehouseID and '
+                                   'warehouseAddressID, then provide the updated addressID, capacity,'
+                                   'addressNum, street, city, and zipCode'}), 400
+
+    try:
+        cursor = conn.cursor()
+        # Update data in warehouse
+        update_query = "UPDATE Warehouse SET capacity = %s WHERE warehouseID = %s"
+        cursor.execute(update_query, (capacity, warehouseID))
+        conn.commit()
+
+        # Update related address data
+        update_query = ("UPDATE Addresses SET addressNum = %s, street = %s, city = %s, zipCode = %s "
+                          "WHERE addressID = %s")
+        cursor.execute(update_query, (addressNum, street, city, zipCode, warehouseAddressID))
+        conn.commit()
+        return jsonify({'message': 'Data updated successfully'}), 201
+    except Exception as e:
+        return jsonify({'message': 'Failed to update data', 'error': str(e)}), 500
+    finally:
+        cursor.close()
+
+
+@app.route("/orders/get_orders", methods=['GET'])
+def get_orders():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Orders")
+    data = cursor.fetchall()
+    if len(data) == 0:
+        return jsonify({'message': "There are no orders in the system."})
+    else:
+        cursor.execute("SELECT * FROM ItemsOrdered ORDER BY orderID")
+        data += cursor.fetchall()
+        cursor.close()
+        return jsonify(data), 200
+
+
+@app.route('/orders/delete_order', methods=['DELETE'])
+def delete_order():
+    data = request.json
+    orderID = data[0]
+
+    if orderID is None:
+        return jsonify({'message': 'orderID is a required field'}), 400
+
+    try:
+        cursor = conn.cursor()
+        delete_query = "DELETE from ItemsOrdered Where orderID = %s"
+        cursor.execute(delete_query, (orderID,))
+
+        delete_query = "Delete from Orders Where orderID = %s"
+        cursor.execute(delete_query, (orderID,))
+        conn.commit()
+        return jsonify({'message': 'Data deleted successfully'}), 200
+    except Exception as e:
+        return jsonify({'message': 'Failed to delete data', 'error': str(e)}), 500
+    finally:
+        cursor.close()
+
+
+@app.route("/item/get_items", methods=['GET'])
+def get_items():
+    cursor = conn.cursor()
+    cursor.execute("SELECT i.itemID, i.itemName, i.itemWeight, i.itemPrice, w.itemQuantity, w.warehouseID "
+                   "FROM Item i INNER JOIN ItemInWarehouse w ON i.itemID = w.itemID")
+    data = cursor.fetchall()
+    if len(data) == 0:
+        return jsonify({'message': "There are no items in the system."})
+    else:
+        # The following command will get all ordered items and display the total amount of those items that are
+        # on order status across all orders
+        cursor.execute("SELECT i.itemName, SUM(o.itemQuantity) AS totalQuantity "
+                       "FROM Item i INNER JOIN ItemsOrdered o "
+                       "ON i.itemID = o.itemID "
+                       "GROUP BY itemName")
+        data += cursor.fetchall()
+        cursor.close()
+        return jsonify(data), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105, debug=True)
 
 # URL Template: "/<table_name>/<function>/<item_or_order_number> (if needed)
